37d1af2a7f3bef963c12ae60069cf2f435e39e97
Praveen Thomas <thomasmethrayil@gmail.com>
2024-07-21 02:57:15 +0530

    fix some keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,18 @@
-local keymap = vim.keymap
+local keys = {
+	{ "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer search" },
+	{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+	{ "<leader>fc", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
+	{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find All Files" },
+	{ "<C-p>", "<cmd>Telescope git_files<cr>", desc = "Git files" },
+	{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
+	{ "<leader>fj", "<cmd>Telescope command_history<cr>", desc = "History" },
+	{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
+	{ "<leader>fl", "<cmd>Telescope lsp_references<cr>", desc = "Lsp References" },
+	{ "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Old files" },
+	{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Ripgrep" },
+	{ "<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "Grep String" },
+	{ "<leader>ft", "<cmd>Telescope treesitter<cr>", desc = "Treesitter" },
+}
 
 local config = function()
 	local telescope = require('telescope')
@@ -15,32 +29,45 @@ local config = function()
 			find_files = {
 				theme = "dropdown",
 				previewer = false,
-        hidden = true,
+        file_ignore_patterns = { "node_modules", ".venv" },
+				additional_args = function(_)
+					return { "--hidden", "--no-ignore-vcs" }
+				end,
+				no_ignore = true,
+				hidden = true,
 			},
 			live_grep = {
 				theme = "dropdown",
 				previewer = false,
+        file_ignore_patterns = { "node_modules", ".venv" },
+				additional_args = function(_)
+					return { "--hidden", "--no-ignore-vcs" }
+				end,
+				hidden = true,
+				no_ignore = true,
 			},
       buffers = {
 				theme = "dropdown",
 				previewer = false,
-			}
+			},
+      extensions = {
+        "fzf",
+      }
 		},
 	})
+  telescope.load_extension("fzf")
 	end
 
 
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.3',
-  lazy = false,
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
+  },
 	config = config,
-	keys = {
-      keymap.set("n", "<leader>fk", ":Telescope keymaps<CR>"),
-      keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>"),
-      keymap.set("n", "<leader>ff", ":Telescope find_files<CR>"),
-      keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>"),
-      keymap.set("n", "<leader>fb", ":Telescope buffers<CR>"),
-	},
+	keys = keys,
 }
